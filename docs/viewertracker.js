d5805d217f2019c546fb860ac41943c0f5c1c5d4
dhavalraichura <dhaval8651000@gmail.com>
2023-01-12 18:48:40 +0530

    Single Page v1.0 Mobile HomeCard

--- docs/viewertracker.js
+++ docs/viewertracker.js
@@ -6,4 +6,20 @@ localStorage.setItem("visits",incrementedVisits);
 document.onreadystatechange = function(){
     document.getElementById("count").innerHTML = incrementedVisits.toString();
     console.log("The site has been visited "+incrementedVisits.toString()+" times");
+
+}
+function myFunction(){
+    var box =  document.getElementById("box");
+    var x = document.getElementById("linklist");
+    if (screen.width <= 480){    
+        if (x.style.display === "flex"){
+            x.style.display = "none";
+            box.style.marginLeft = "80vw";
+        } else {
+            x.style.display = "flex";
+            box.style.marginLeft = "8.75vw";
+            x.style.marginLeft = "40vw";
+            x.style.marginTop = "20vh";
+        }
+    }
 }
